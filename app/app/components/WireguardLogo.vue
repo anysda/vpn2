40257c1cbdd4a35717cad6cd1471280366c6db8d
anysda <anysdaw@gmail.com>
2026-05-20 01:34:03 +0300

    WireguardLogo: switch to real brand SVG asset
    Drop the hand-rolled "red square + face" approximation; the component
    now points at /wireguard.svg in public/. The actual brand SVG (dropped
    in by the user) lives there.

--- app/app/components/WireguardLogo.vue
+++ app/app/components/WireguardLogo.vue
@@ -1,16 +1,3 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 64 64"
-    role="img"
-    aria-label="WireGuard"
-  >
-    <rect width="64" height="64" rx="14" fill="#88171A"/>
-    <g fill="#fff" fill-rule="evenodd">
-      <path d="M32 10 C20.4 10 11 19.4 11 31 V44 C11 50 17 54 23 54 H41 C47 54 53 50 53 44 V31 C53 19.4 43.6 10 32 10 Z M32 14 C41.4 14 49 21.6 49 31 V44 C49 47.5 45.5 50 41 50 H23 C18.5 50 15 47.5 15 44 V31 C15 21.6 22.6 14 32 14 Z"/>
-      <circle cx="25" cy="30" r="3.2"/>
-      <circle cx="39" cy="30" r="3.2"/>
-      <path d="M22 40 C24.5 43 28 44.5 32 44.5 C36 44.5 39.5 43 42 40 L40 38.5 C38 41 35 42 32 42 C29 42 26 41 24 38.5 Z"/>
-    </g>
-  </svg>
+  <img src="/wireguard.svg" alt="WireGuard" draggable="false">
 </template>
